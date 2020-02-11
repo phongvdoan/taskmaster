8e5f497e7543c0992015450dde5056afe0a1cfec
Phong <phongdoan.dev@gmail.com>
2020-02-11 15:19:48 -0800

    added back buttons and Toast

--- app/src/main/java/com/phongvdoan/taskmaster/AddTask.java
+++ app/src/main/java/com/phongvdoan/taskmaster/AddTask.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddTask extends AppCompatActivity {
 
@@ -19,8 +20,11 @@ public class AddTask extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                TextView submitText = findViewById(R.id.textView4);
-                submitText.setVisibility(View.VISIBLE);
+//                TextView submitText = findViewById(R.id.textView4);
+////                submitText.setVisibility(View.VISIBLE);
+                //Toasts
+                Toast submitToast = Toast.makeText(getApplicationContext(), "Submitted!", Toast.LENGTH_SHORT);
+                submitToast.show();
 
             }
         });
