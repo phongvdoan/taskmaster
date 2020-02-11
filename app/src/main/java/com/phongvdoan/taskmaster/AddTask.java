1778900d6797815b4f676bfe467452786fdcf55c
Phong <phongdoan.dev@gmail.com>
2020-02-11 11:03:22 -0800

    Readme completed

--- app/src/main/java/com/phongvdoan/taskmaster/AddTask.java
+++ app/src/main/java/com/phongvdoan/taskmaster/AddTask.java
@@ -3,6 +3,9 @@ package com.phongvdoan.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AddTask extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class AddTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        Button submitButtom = findViewById(R.id.button);
+        submitButtom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView submitText = findViewById(R.id.textView4);
+                submitText.setVisibility(View.VISIBLE);
+
+            }
+        });
     }
 }
